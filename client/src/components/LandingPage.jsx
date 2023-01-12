7173ef496465b2a06c8d66901d3d34f16e13ac50
Cinthyavmf <cinthyavmf@gmail.com>
2023-01-11 23:32:18 -0300

    styles landing page

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -5,18 +5,14 @@ import style from './LandingPage.module.css';
 const LandingPage = function() {
     return (
         <div className = {style.landing}>
-            <div className={style.container}>
-                <h1 className={style.title}>Henry Recipes</h1>
-                <h3>"let food be thy medicine and medicine be thy food."</h3>
-                <h4 className={style.autor}>Hippocrates</h4>
-
-                    <button className={style.start}>
-                        <Link to= '/home'style = {{textDecoration:"none", color:"white"}}>
-                            Start!
-                        </Link>
-                    </button>
-
-            </div>
+            <h1 className={style.title}>Henry Recipes</h1>
+            <h3>"let food be thy medicine and medicine be thy food."</h3>
+            <h4 className={style.autor}>Hippocrates</h4>
+            <button className={style.start}>
+                <Link to= '/home'style = {{textDecoration:"none", color:"white"}}>
+                    Start!
+                </Link>
+            </button>
         </div>
     )
 }
