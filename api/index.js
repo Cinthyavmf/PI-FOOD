2b7432c7ccd7a0456cf3bf361e628fde55422a15
Cinthyavmf <cinthyavmf@gmail.com>
2023-01-22 09:45:33 -0300

    require dotenv

--- api/index.js
+++ api/index.js
@@ -19,10 +19,11 @@
 
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+require("dotenv").config();
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT || 3045, () => {
-    console.log('%s listening at ' + process.env.PORT); // eslint-disable-line no-console
+  server.listen(process.env.PORT, () => {
+    console.log('%s listening at port'); // eslint-disable-line no-console
   });
 });
