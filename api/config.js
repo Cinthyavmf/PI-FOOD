fe497835dbd8c0ae6659b2378b965cd3e7001b93
Cinthyavmf <cinthyavmf@gmail.com>
2023-01-22 14:14:03 -0300

    exports

--- api/config.js
+++ api/config.js
@@ -1,9 +1,11 @@
-export const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
-export const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.API_KEY;
 
-export const DB_USER = process.env.DB_USER || 'postgres';
-export const DB_PASSWORD = process.env.DB_PASSWORD;
-export const DB_HOST = process.env.DB_HOST || 'localhost';
-export const DB_NAME = process.env.DB_NAME || 'food';
-export const DB_PORT = process.env.DB_PORT || 5432
+const DB_USER = process.env.DB_USER || 'postgres';
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_NAME = process.env.DB_NAME || 'food';
+const DB_PORT = process.env.DB_PORT || 5432
+
+module.exports = {PORT, API_KEY, DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT};
