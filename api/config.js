2ce15f3cb5148a497670a451830ca12fd052c43f
Cinthyavmf <cinthyavmf@gmail.com>
2023-01-22 14:09:13 -0300

    database

---
+++ api/config.js
@@ -0,0 +1,9 @@
+export const PORT = process.env.PORT || 3001;
+
+export const API_KEY = process.env.API_KEY;
+
+export const DB_USER = process.env.DB_USER || 'postgres';
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_HOST = process.env.DB_HOST || 'localhost';
+export const DB_NAME = process.env.DB_NAME || 'food';
+export const DB_PORT = process.env.DB_PORT || 5432
--- api/index.js
+++ api/index.js
@@ -19,11 +19,11 @@
 
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-require("dotenv").config();
+const { PORT } = require('./config.js')
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(PORT, () => {
     console.log('%s listening at port'); // eslint-disable-line no-console
   });
 });
